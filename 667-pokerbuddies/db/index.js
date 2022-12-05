4a3a2b9265e4201f4bc2a8d4c9e7bf8f8c12b308
celiarr <78006504+celiarr@users.noreply.github.com>
2022-12-04 23:16:20 -0800

    Update index.js

--- 667-pokerbuddies/db/index.js
+++ 667-pokerbuddies/db/index.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-const db = pgp('postgres://stats_99ty_user:RFbyIf3x5WANM3iae1kSWuL0dLIHvAvA@dpg-cdel6r4gqg4d3ggr0fr0-a.oregon-postgres.render.com/stats_99ty?ssl=true. ');
+const db = pgp('postgres://stats_99ty_user:RFbyIf3x5WANM3iae1kSWuL0dLIHvAvA@dpg-cdel6r4gqg4d3ggr0fr0-a.oregon-postgres.render.com/stats_99ty?ssl=true');
     
 db.connect()
     .then(obj => {
